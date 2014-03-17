9c21226cb0a425f31fd8e35cb2009a18bd2450c0
Matias Zanolli <z_killemall@yahoo.com>
2014-03-17 17:23:36 -0300

    el coso del downloooooo

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,8 @@
 
 // var MovieModel = require('../models/movie');
 var http = require('http');
-
+var movie = require('models/movie');
+console.log(movie);
 module.exports = function (app) {
 	var model = {};
     app.get('/', function (req, res) {
@@ -18,12 +19,38 @@ module.exports = function (app) {
 		    res.on('end', function() {
 		        var jsonResponse = JSON.parse(body)
 		        model = jsonResponse;
-		        console.log(model.MovieList[0]);
+		        //console.log(model.MovieList[0]);
 		    });
 		}).on('error', function(e) {
 		      console.log("Got error: ", e);
 		});
 		res.render('index', model);
+    });
+    app.get('/download/:id', function(req, res, next) {
+	    var id = req.params.id;
+	    if (id) {
+	    	var url = 'http://yts.re/api/movie.json?id='+id;
+	    	http.get(url, function(res) {
+			    var body = '';
+
+			    res.on('data', function(chunk) {
+			        body += chunk;
+			    });
+
+			    res.on('end', function() {
+			        var jsonResponse = JSON.parse(body)
+			        console.log(jsonResponse);
+			    });
+			}).on('error', function(e) {
+			      console.log("Error loading movie data: ", e);
+			});
+	    } else {
+	        next();
+	    }
+	}, function (req, res) {
+    	console.log(id);
+		var model = {};
+		res.render('download', model);
     });
 
 };
--- models/movie.js
+++ models/movie.js
@@ -1,8 +1,30 @@
 'use strict';
 
 
-module.exports = function MovieModel() {
-    return {
-        movies: []
-    };
+var http = require('http');
+
+var MovieModel = function () {
+	var load = function(req, res, next) {
+	    var id = req.params.id;
+	    if (id) {
+	    	http.get(url, function(res) {
+			    var body = '';
+
+			    res.on('data', function(chunk) {
+			        body += chunk;
+			    });
+
+			    res.on('end', function() {
+			        var jsonResponse = JSON.parse(body)
+			        console.log(jsonResponse);
+			    });
+			}).on('error', function(e) {
+			      console.log("Error loading movie data: ", e);
+			});
+	    } else {
+	        next();
+	    }
+	}
 };
+
+module.exports = new MovieModel();
